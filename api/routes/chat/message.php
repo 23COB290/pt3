005d73392439a3976ec20606ef707f90b78be1c1
aaix <55102797+aaix@users.noreply.github.com>
2024-04-25 16:04:29 +0100

    more framework for messages

--- api/routes/chat/message.php
+++ api/routes/chat/message.php
@@ -4,18 +4,31 @@ require_once("lib/object_commons/object_route.php");
 
 const MESSAGE_MESSAGE_CHECKS = [
     "DELETE"=>[
-
+        "duo_arg",
+        "channel_exists",
+        "user_in_channel",
+        "message_exists",
+        "author_authored_message"
     ],
     "GET"=>[
-
+        "duo_arg",
+        "channel_exists",
+        "user_in_channel",
+        "message_exists",
     ],
     "PATCH"=>[
-
+        "duo_arg",
+        "channel_exists",
+        "user_in_channel",
+        "message_exists",
+        "author_authored_message"
     ],
     "POST"=>[
-
+        "duo_arg",
+        "channel_exists",
+        "user_in_channel",
     ],
-    
+
 ];
 
 
@@ -24,8 +37,59 @@ function r_chat_message(RequestContext $ctx, string $args) {
 }
 
 function r_chat_messages(RequestContext $ctx, string $args) {
+    object_check_channel_exists($ctx, $args);
+    object_check_user_in_channel($ctx, $args);
 
+    // needs pagination
 }
+
+function _new_message(RequestContext $ctx, array $body, array $url_specifiers) {
+    $author_id = $ctx->session->hex_associated_user_id;
+    $channel_id = $url_specifiers[0];
+
+    $msgID = generate_uuid();
+    $createdAt = timestamp();
+    $content = $body["content"];
+
+
+    if (db_generic_new(
+        TABLE_MESSAGES ,
+        [
+            $msgID,
+            $channel_id,
+            $author_id,
+            $createdAt,
+            $content
+        ],
+        "sssis"
+    )) {
+
+        $body["msgID"] = $msgID;
+        $body["messageCreatedAt"] = $createdAt;
+        $body["author"] = $author_id;
+        $body["channelID"] = $channel_id;
+
+        respond_ok(parse_database_row($body, TABLE_MESSAGES));
+    } else {
+        respond_internal_error(ERROR_DB_INSERTION_FAILED);
+    }
+}
+
+
+function _delete_message(RequestContext $ctx, array $url_specifiers) {
+    db_message_delete($url_specifiers[1]);
+    respond_no_content();
+}
+
+function _edit_message(RequestContext $ctx, array $body, array $url_specifiers) {
+
+    _use_common_edit(TABLE_MESSAGES, $body, $url_specifiers);
+}
+
+function _fetch_message(RequestContext $ctx, array $url_specifiers) {
+    respond_ok($ctx->message);
+}
+
 
 register_route(new Route(
     ["GET"],
